e73a917fd0c862d9a0e212ea7f6b450453f1d05d
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-13 19:08:12 -0700

    define structs

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,24 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::path::Path;
+
+struct PromptResult {
+    #[allow(dead_code)]
+    serifu: String,
+    #[allow(dead_code)]
+    paths: Vec<String>,
+}
+
+#[derive(Debug)]
+struct MatchResult {
+    #[allow(dead_code)]
+    path: String,
+    #[allow(dead_code)]
+    serifu: String,
+    #[allow(dead_code)]
+    line: u32,
+}
+
 fn main() {
     println!("Hello, world!");
 }
